f0b722389005531cbc6b081b74c0c43613762a18
nightink <chceyes@gmail.com>
2016-01-21 21:19:05 +0800

    feat: add agent

--- test/easy.test.js
+++ test/easy.test.js
@@ -1,49 +1,51 @@
 
-const request = require('..');
+'use strict';
+
+const ws = require('..');
 const should = require('should');
 
 describe('easy get test', function() {
 
   describe('request http', function() {
-    it('should status code 200 get request http://www.baidu.com', function() {
+    it('should status code 200 get request http://www.baidu.com', function(done) {
 
-      request('http://www.baidu.com', function(err, response) {
+      ws.request('http://www.baidu.com').then(function(response) {
         response.statusCode.should.eql(200);
-      });
+      }).then(done);
     });
 
-    it('should return text is right when get http://www.baidu.com', function() {
-      request('http://www.baidu.com', function(err, response) {
+    it('should return text is right when get http://www.baidu.com', function(done) {
+      ws.request('http://www.baidu.com').then(function(response) {
         response.html.should.containEql('<title>百度一下，你就知道</title>');
-      });
+      }).then(done);
     });
   });
 
-  describe('request http options {}', function() {
+  describe('request http options {}', function(done) {
     it('should return text is right when get http://www.baidu.com', function() {
-      request('http://www.baidu.com', {}, function(err, response) {
+      ws.request('http://www.baidu.com', {}).then(function(response) {
         response.html.should.containEql('<title>百度一下，你就知道</title>');
-      });
+      }).then(done);
     });
   });
 
   describe('get https', function() {
-    it('should status code 200 get request https://www.baidu.com', function() {
-      request('https://www.baidu.com', function(err, response) {
+    it('should status code 200 get request https://www.baidu.com', function(done) {
+      ws.request('https://www.baidu.com').then(function(response) {
         response.statusCode.should.eql(200);
-      });
+      }).then(done);
     });
 
-    it('should return text is right when get https://www.baidu.com', function() {
-      request('https://www.baidu.com', function(err, response) {
+    it('should return text is right when get https://www.baidu.com', function(done) {
+      ws.request('https://www.baidu.com').then(function(response) {
         response.html.should.containEql('<title>百度一下，你就知道</title>');
-      });
+      }).then(done);
     });
   });
 
   describe('get fail error', function() {
     it('get https://www.alipay.co', function(done) {
-      request('https://www.alipay.co', function(err) {
+      ws.request('https://www.alipay.co').catch(function(err) {
         should.exist(err);
         done();
       });
